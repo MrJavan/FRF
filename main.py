83338da6f99fb9febd7b498164d230cffee7f5f5
MrJavan <am1njavan@proton.me>
2024-11-30 09:58:10 +0330

    clean code

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-from clear import clear
-clear()
-# x = -2
-# y = -1
-# c = 0 
-# n = 3
-# x = -1
-
-# def first(n):
-#     if n != x:
-#         c += 1
-#         return first(n - 1)
-#     else:
-#         print(n + 2)
-#         print(c)
-#         return n + 2
-
-# def second(n):
-#     if n != y:
-#         return second(n - 1)
-#     else:
-#         print(n + 1)
-#         return n + 1
 
 def f(n):
     if n == 0:
