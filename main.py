80964c2983d759bc215010f0ee25a8e1631e641b
MrJavan <am1njavan@proton.me>
2024-11-30 11:24:57 +0330

    fix bugs

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 def f(n):
-    if n == 0:
+    if n < 0:
+        return "Not valid!"        
+    elif n == 0:
         return 0
-    elif n == -1:
+    elif n == 1:
         return 1
     else:
         return f(n - 2) + f(n - 1)
 
 print(f(int(input("Number: "))))
-     
